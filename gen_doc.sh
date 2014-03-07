fa7b36ee80381d7c9336dffc204b92c90ae8a1c6
Nikita Afanasenko <nikita@afanasenko.name>
2014-03-07 15:56:54 +0400

    use relative path in gen_doc script

--- gen_doc.sh
+++ gen_doc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "## Keys\n"
-cat vimrc | grep '" key ' | sed 's/ *" key\(.*\)$/-\1/'
+cat `dirname $0`/vimrc | grep '" key ' | sed 's/ *" key\(.*\)$/-\1/'
 
 echo "\n## Commands\n"
-cat vimrc | grep '" cmd ' | sed 's/ *" cmd\(.*\)$/-\1/'
+cat `dirname $0`/vimrc | grep '" cmd ' | sed 's/ *" cmd\(.*\)$/-\1/'
